f01825cb442fb93e1fc9ee09ff13b9f575f605ee
mtthwwl <ravarthurr@gmail.com>
2024-12-18 20:25:13 +0700

    Countdown Timer Wrapped

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const giveaway = document.querySelector('.giveaway');
-const deadline = document.querySelector('.deadline');
-const items = document.querySelectorAll('.deadline-format h4');
-
-let tempDate = new Date();
-
-let tempYear = tempDate.getFullYear();
-let tempMonth = tempDate.getMonth(); // month has issue with 0-11
-let tempDay = tempDate.getDate();
-
-const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
-
-const year = futureDate.getFullYear();
-const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes();
-const month = futureDate.getMonth();
-const Date = futureDate.getDate();
-
 const months = [
     'January',
     'February',
@@ -29,4 +11,40 @@ const months = [
     'October',
     'November',
     'December'
-]
+];
+
+const weekday = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let tempDate = new Date();
+
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();   // month have issue with 0-11
+let tempDay = tempDate.getDate();
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const month = futureDate.getMonth();
+const date = futureDate.getDate();
+
+console.log(futureDate);
+console.log(months);
+
+// month = months[month];
+//weekday = weekday[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
